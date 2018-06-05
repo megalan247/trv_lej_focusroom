f41813be1608e8b88edfeb976c0aa296624f4e37
Rory Shanks <megalan247@users.noreply.github.com>
2018-06-05 14:30:01 +0200

    Removed AM/PM from 24hr format time

--- js/app.js
+++ js/app.js
@@ -2,11 +2,10 @@ function formatReadableDate() {
 var d = new Date(),
     minutes = d.getMinutes().toString().length == 1 ? '0'+d.getMinutes() : d.getMinutes(),
     hours = d.getHours().toString().length == 1 ? '0'+d.getHours() : d.getHours(),
-    ampm = d.getHours() >= 12 ? 'PM' : 'AM',
     months = ['January','February','March','April','May','June','July','August','September','October','November','December'],
     days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
 	var readableDate = days[d.getDay()]+', '+months[d.getMonth()]+' '+d.getDate()+' '+d.getFullYear();
-	var readableTime = hours+':'+minutes+ampm;
+	var readableTime = hours+':'+minutes;
 	document.getElementById('date').innerHTML = readableDate;
 	document.getElementById('time').innerHTML = readableTime;
 	
