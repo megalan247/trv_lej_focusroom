0ef5a6b48b38ff1eb915110ed43749eae48ccdd4
Rory Shanks <megalan247@users.noreply.github.com>
2018-06-05 10:05:42 +0200

    Added date adn time

--- js/app.js
+++ js/app.js
@@ -1,5 +1,18 @@
-function bookDesk(deskID) {
+function formatReadableDate() {
+var d = new Date(),
+    minutes = d.getMinutes().toString().length == 1 ? '0'+d.getMinutes() : d.getMinutes(),
+    hours = d.getHours().toString().length == 1 ? '0'+d.getHours() : d.getHours(),
+    ampm = d.getHours() >= 12 ? 'PM' : 'AM',
+    months = ['January','February','March','April','May','June','July','August','September','October','November','December'],
+    days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+	var readableDate = days[d.getDay()]+', '+months[d.getMonth()]+' '+d.getDate()+' '+d.getFullYear();
+	var readableTime = hours+':'+minutes+ampm;
+	document.getElementById('date').innerHTML = readableDate;
+	document.getElementById('time').innerHTML = readableTime;
 	
+}
+
+function bookDesk(deskID) {
 	if (document.getElementById(deskID).style.backgroundColor == "red") {
 		localStorage.setItem(deskID + "-booktime", (Number(localStorage.getItem(deskID + "-booktime")) + 1800));
 		document.getElementById(deskID + "-book").innerHTML = new Date(localStorage.getItem(deskID + "-booktime") * 1000).toISOString().substr(11, 8);
@@ -17,8 +30,6 @@ function bookDesk(deskID) {
 				localStorage.setItem(deskID + "-booktime", (Number(localStorage.getItem(deskID + "-booktime")) - 1));
 				document.getElementById(deskID + "-book").innerHTML = new Date(localStorage.getItem(deskID + "-booktime") * 1000).toISOString().substr(11, 8);
 			}
-
-			
 		}, 1000);
 	}
 }
@@ -29,4 +40,8 @@ function checkoutDesk(deskID) {
 	document.getElementById(deskID + "-checkout").style.display = "none";
 	document.getElementById(deskID + "-book").innerHTML = "Tap to Book";
 	document.getElementById(deskID + "-status").innerHTML = "Available";
+}
+
+window.onload = function() {            
+    setInterval("formatReadableDate()",500)
 }
